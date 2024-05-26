39493f0b1d244cdabba6b5291166e1fd0215aa4b
Goodcat141 <159024391+Goodcat141@users.noreply.github.com>
2024-05-26 22:23:39 +0300

    Add files via upload

---
+++ Laba5/script F.js
@@ -0,0 +1,22 @@
+"use strict";
+let a = 1;
+let b = 2;
+let c = 3;
+
+let d = 1, e = 2, f = 3;
+
+let g, h, i; // объявляем все 3 переменные
+
+// Присваиваем переменным значения:
+g = 1;
+h = 2;
+i = 3;
+alert(a);
+alert(b);
+alert(c);
+alert(d);
+alert(e);
+alert(f);
+alert(g);
+alert(h);
+alert(i);
